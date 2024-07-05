a77c8ba45c82c7b2c5e0d9d342f3a46fc3ef17bd
alireza kargar <alirezakargarsuport@gmail.com>
2024-07-05 14:55:11 +0330

    add Text animation

--- src/sections/home/home-view.tsx
+++ src/sections/home/home-view.tsx
@@ -8,7 +8,10 @@ import "splitting/dist/splitting.css";
 import "splitting/dist/splitting-cells.css";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, BoxProps, Button, Container, Typography } from "@mui/material";
+import { MotionViewport, varFade } from "src/components/animate";
+import { m, MotionProps } from 'framer-motion';
+import { Variant } from "@mui/material/styles/createTypography";
 // const Splitting = dynamic(() => import('splitting'), {
 //     ssr: false
 // })
@@ -32,11 +35,21 @@ export default function HomeView() {
                     overflow: 'auto',
                     height: '100vh'
                 }}>
-                    <Box mt={26} textAlign={'center'}>
-                        <Typography variant="h2" width={1}>We create digital magic</Typography>
-                        <Typography variant="h5" width={1}>Beauty, creativity, and innovation unleashed.</Typography>
-                        <Button variant="contained" color="primary" sx={{ mt: 4 }}>Learn more</Button>
-                    </Box>
+                    <Container component={MotionViewport}>
+                        <Box mt={26} textAlign={'center'} justifyContent={'center'}>
+                            <TextAnimate text="We create digital magic" variants={varFade().inUp}
+                                px={0.5}
+                                sx={{ width: 1, textAlign: 'center', justifyContent: 'center' }} />
+                            <TextAnimate text="Beauty, creativity, and innovation unleashed."
+                                split=" "
+                                px={0.25}
+                                variant="h6"
+                                variants={varFade().inUp} />
+                            <Typography variant="h5" width={1}></Typography>
+                            <Button variant="contained" color="primary" sx={{ mt: 4 }}>Learn more</Button>
+                        </Box>
+                    </Container>
+
                 </section>
                 <Box width={1} bgcolor={'white'} overflow={'auto'} display={'none'}>
                     <h1>hello world</h1>
@@ -103,4 +116,35 @@ export default function HomeView() {
             <section></section>
         </div>
     )
+}
+
+type TextAnimateProps = BoxProps &
+    MotionProps & {
+        text: string;
+        split?: string;
+        variant?: Variant
+        px: number;
+    };
+
+function TextAnimate({ text, variants, variant = "h2", split = " ", px, sx, ...other }: TextAnimateProps) {
+    return (
+        <Box
+            component={m.div}
+            sx={{
+                typography: variant,
+                overflow: 'hidden',
+                display: 'inline-flex',
+                ...sx,
+            }}
+            {...other}
+        >
+            {text.split(split).map((letter, index) => (
+                <m.span key={index} variants={variants || varFade().inUp}>
+                    <Box px={px} key={index}>
+                        {letter}
+                    </Box>
+                </m.span>
+            ))}
+        </Box>
+    );
 }
