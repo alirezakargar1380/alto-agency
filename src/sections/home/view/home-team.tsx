bb0ff14c91bb998a4e14de36340bd4da2630eca9
alireza kargar <alirezakargarsuport@gmail.com>
2026-05-24 23:36:30 +0330

    update my gif

--- src/sections/home/view/home-team.tsx
+++ src/sections/home/view/home-team.tsx
@@ -28,7 +28,7 @@ const teams: ITeam[] = [
     {
         name: "Alireza Kargar",
         role: "Full Stack Web Developer",
-        src: "/assets/images/team/ME_2.gif",
+        src: "/assets/images/team/ME_2_2.gif",
         description: "i'm alireza kargar with over 10 years of experience in web development. \n i'm a full stack web developer with a focus on building responsive and user-friendly websites. i have experience in a variety of programming languages and frameworks, including html, css, javascript, react, and node.js. i'm also familiar with a variety of web development tools and technologies, including git, github, and webpack. i'm a quick learner and i'm always looking for new challenges. i'm a team player and i'm always willing to help others. i'm a problem solver and i'm always looking for new ways to improve my skills. i'm a creative thinker and i'm always looking for new ways to improve my skills. i'm a problem solver and i'm always looking for new ways to improve my skills. i'm a creative thinker and i'm always looking for new ways to improve my skills. i'm a problem solver and i'm always looking for new ways to improve my skills. i'm a creative thinker and i'm always looking for new ways to improve my skills. i'm a problem solver and i'm always looking for new ways to improve my skills.",
         links: {
             github: "",
